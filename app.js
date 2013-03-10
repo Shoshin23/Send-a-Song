4a4e6345c0d47044ad1540854783362ede7942f1
Karthik Kannan <karthikk.314@gmail.com>
2013-03-11 02:59:44 +0530

    updated app.js with code for Soundcloud API and Twilio's API

--- app.js
+++ app.js
@@ -3,11 +3,39 @@
  * Module dependencies.
  */
 
+var config = {
+	port: process.env.VMC_APP_PORT || 8080,
+
+	title: "Plush",
+
+	twilio: {
+		account_sid: "xxx",
+		auth_token: "xxx",
+		number: "xxx"
+	},
+
+	soundcloud: {
+		id: "xxx"
+	}
+};
+
+
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , request = require('request')
+  , util = require('util')
+  , Twilio = require('twilio-js')
+
+  //Setting up Twilio Auth Deails
+  //Get your Twilio account details and sub them in config
+
+  Twilio.AccountSid = config.twilio.account_sid;
+  Twilio.AuthToken = config.twilio.auth_token;
+
+
 
 var app = express();
 
@@ -27,8 +55,47 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//Get Soundcloud id from Soundcloud URL given by the user.
+//
+function getSoundcloudIDfromURL(url,callback) {
+	var url = "https://api.soundcloud.com/resolve.json?client_id="+config.soundcloud.id+"&url="+url;
+	request({url:url,json:true},function(error,response,data) {
+		if(!error && response.statusCode == 200) {
+			callback(data.id);
+		}
+	})
+}
+
+//Render the page
+
+app.get("/",function(req,res) {
+	var socketID = Math.random()*1000000000 + (new Date()).getTime();
+
+	//if form has been submitted
+	if(req.query.url! == undefined) {
+		var url = req.query.url
+
+	//get id from the url
+	getSoundcloudIDfromURL(url,function(id) {
+		
+		//create the phone call
+		var url = "http://wemakeawesomesh.it/soundcloudID/twiml.php?id="+id+"&socket_id="+socketID;
+		Twilio.Call.create({
+			to: req.query.to,
+			from:config.twilio.number, //your Twilio account number
+			url: url
+		}, function(e) {
+			res.render('index',{title: config.title, phonecall: true, socketID: socketID})
+		});
+	})
+	}
+	else {
+		res.render('index', {title: config.title, phonecall: false})
+	}
+})
+
+//app.get('/', routes.index);
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
