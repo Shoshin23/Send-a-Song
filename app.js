d4d1517d49a54a6fe9e0949c452cd043a744e95f
Karthik Kannan <karthikk.314@gmail.com>
2013-03-23 20:25:33 +0530

    cleared a routes bug.

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var config = {
 
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  //, routes = require('./routes')
+  //, user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , request = require('request')
